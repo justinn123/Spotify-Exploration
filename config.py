f66d8d4cc6acfabd5545ac6474a801a09ed882d6
Justin Nguyen <justinn123123123@gmail.com>
2025-01-06 20:21:26 -0500

    updated requirements.txt

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SECRET_KEY = os.urandom(64)
     SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
     SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
-    SPOTIPY_REDIRECT_URI = "http://localhost:5000/callback"
+    SPOTIPY_REDIRECT_URI = "spotify-exploration-rnfejb51x-ragingazians-projects.vercel.app/callback"
     SPOTIPY_SCOPE = '''user-top-read user-library-read 
     playlist-read-private 
     user-read-playback-state 
