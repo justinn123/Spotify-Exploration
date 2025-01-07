22968a051d0677090e019907898cdea67b048679
Justin Nguyen <justinn123123123@gmail.com>
2025-01-06 19:19:06 -0500

    using env file for confidential info

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+from dotenv import load_dotenv
 import os
-from credentials import client_id, client_secret
+
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.urandom(64)
-    SPOTIPY_CLIENT_ID = client_id
-    SPOTIPY_CLIENT_SECRET = client_secret
+    SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
+    SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
     SPOTIPY_REDIRECT_URI = "http://localhost:5000/callback"
     SPOTIPY_SCOPE = '''user-top-read user-library-read 
     playlist-read-private 
