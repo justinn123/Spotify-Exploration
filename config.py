917930cdeb27083897e339afa0de0d139ceff5c7
Justin Nguyen <justinn123123123@gmail.com>
2025-01-06 20:27:18 -0500

    updated redirect uri

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SECRET_KEY = os.urandom(64)
     SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
     SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
-    SPOTIPY_REDIRECT_URI = "spotify-exploration-rnfejb51x-ragingazians-projects.vercel.app/callback"
+    SPOTIPY_REDIRECT_URI = "https://spotify-exploration-1j6strk9o-ragingazians-projects.vercel.app/callback"
     SPOTIPY_SCOPE = '''user-top-read user-library-read 
     playlist-read-private 
     user-read-playback-state 
